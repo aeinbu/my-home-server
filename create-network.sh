a837293a230e010de9d0b8c4d04d7730fc75cf4b
Arjan Einbu <arjan@einbu.no>
2026-01-31 01:16:49 +0100

    Add script to create macvlan network

---
+++ create-network.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create macvlan network for
+docker network create \
+  --driver macvlan \
+  --subnet=192.168.1.0/24 \
+  --gateway=192.168.1.1 \
+  --ip-range=192.168.1.128/25 \
+  -o parent=enp3s0f1 \
+  my-macvlan-network
+
+echo "Network my-macvlan-network created successfully"
